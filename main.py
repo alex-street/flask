ff8e6a2d0490feca6d7d825a5a87266b65668dda
alexis <alexis.bradstreet01@gmail.com>
2025-05-31 18:03:55 +0100

    Update main.py

--- main.py
+++ main.py
@@ -10,11 +10,11 @@ def index():
 def creatematchup():
   return render_template('creatematchup.html')
 def savematchup():
-    try:
-        # Get form data
-        yourTeamName = request.form.get('yourTeamName', '').strip()
-        opponentTeamName = request.form.get('opponentTeamName', '').strip()
-        matchDate = request.form.get('matchDate', '')
+  try:
+      # Get form data
+      yourTeamName = request.form.get('yourTeamName', '').strip()
+      opponentTeamName = request.form.get('opponentTeamName', '').strip()
+      matchDate = request.form.get('matchDate', '')
 
 if __name__ == '__main__':
   app.run(port=5000)
