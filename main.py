93a9f65b88c80b33b5c339f67761450133acf21b
Alexis Bradstreet <alexisbradstreet@Alexiss-MBP-2.lan>
2025-06-01 16:42:47 +0100

    debug

--- main.py
+++ main.py
@@ -117,6 +117,7 @@ def saveprobabilities():
                 VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
                 RETURNING id
             """
+    print(possibilities)
     cursor.execute(insert_query, (
                 matchup_id,
                 str(possibilities[0][0])+","+str(possibilities[0][1])+","+str(possibilities[0][2])+","+str(possibilities[0][3])+","+str(possibilities[0][4])+","+str(possibilities[0][5])+","+str(possibilities[0][6])+","+str(possibilities[0][7]),
