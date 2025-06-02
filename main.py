9edb4dfac47e1811fbf2ed9df246647e23206afa
Alexis Bradstreet <alexisbradstreet@Alexiss-MBP-2.lan>
2025-06-02 02:45:24 +0100

    edit

--- main.py
+++ main.py
@@ -138,10 +138,15 @@ def viewmatchups():
 
 @app.route('/editmatchup/<int:matchup_id>')
 def editmatchup(matchup_id):
+   return render_template('editmatchup.html', id=matchup_id)
+
+@app.route('/editmatchup-round1/<int:matchup_id>')
+def editmatchup(matchup_id):
    query = "SELECT home1, home2, home3, home4, home5, opponent1, opponent2, opponent3, opponent4, opponent5, opponent6, opponent7, opponent8 FROM matchup WHERE id = %s"
    cursor.execute(query, (matchup_id,))
-   h = cursor.fetchall()[0][:5]
-   o = cursor.fetchall()[0][5:]
+   info = cursor.fetchone()
+   h = info[:5]
+   o = info[5:]
    return render_template('editmatchup.html', id=matchup_id, homeplayers=h, opponentplayers=o)
 
 if __name__ == '__main__':
